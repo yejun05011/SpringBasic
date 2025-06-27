89f957110d8eb83bf0ba9c952a1769875f4f6b82
yejun05011 <dpwnsdl0501@naver.com>
2025-06-27 22:00:42 +0900

    "프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점"까지 완료

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -30,7 +30,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
